22db9ab8733d4999a90060e83cfadb47018dec27
rockycsumb <37380272+rockycsumb@users.noreply.github.com>
2018-11-28 13:12:53 -0800

    ADDED IMG URLS

--- heroku_74152a32ba521c4.sql
+++ heroku_74152a32ba521c4.sql
@@ -107,28 +107,50 @@ CREATE TABLE `f_product` (
 --
 
 INSERT INTO `f_product` (`productID`, `productName`, `productDescription`, `productImage`, `brandID`, `categoryID`, `price`, `likesID`) VALUES
-(21, 'Model 3', '310 mile range, 0-60 mpg 3.5 seconds', '', 21, 51, '45000', 1),
-(31, 'Model S', '315 range, 518 horsepower, 6 hour ', '', 21, 51, '77000', 1),
-(41, 'Model X', '289 mile range, 518 horsepower, 0-', '', 21, 51, '83000', 1),
-(51, 'Zero S', 'Top speed 95mph, Range ', '', 31, 61, '11000', 1),
-(61, 'Zero SR', 'Top speed 95mph', '', 31, 61, '16500', 1),
-(71, 'Zero DS', 'Top speed 98 mph, Rang', '', 31, 61, '11000', 1),
-(81, 'Zero DSR', '102 mph top speed Range', '', 31, 61, '16500', 1),
-(91, 'Zero FX', 'Top Speed 85 mpg, Range:', '', 31, 61, '9000', 1),
-(101, 'Zero FXS', 'Top Speed 85 mpg, Range ', '', 31, 61, '9000', 1),
-(111, 'Bamboo GTX Street', 'Top Speed 26mph Ran', '', 41, 71, '1560', 1),
-(121, 'Bamboo One', 'Top Speed 26 Mph Range 21 ', '', 41, 71, '800', 1),
-(131, 'GT Carbon Series', 'Top Speed 26mph Rang', '', 41, 71, '1960', 1),
-(141, 'E Prime', 'Electric Scooter 15 mph speed Range ', '', 51, 81, '379', 1),
-(151, 'E300', 'Electric Scooter 15 mph speed Range 45 ', '', 51, 81, '279', 1),
-(161, 'E150', 'Electric Scooter 10 mph speed, Range 40', '', 51, 81, '159', 1),
-(171, 'Eco Smart Metro', 'Electric Scooter 18mph Speed', '', 51, 81, '469', 1),
-(181, 'E200S', 'Electric Scooter Seated 12 mph speed R', '', 51, 81, '259', 1),
-(191, 'Galaxy ST', '50 mile range', '', 61, 91, '2999', 1),
-(201, 'Aurora', '40 mile range', '', 61, 91, '2999', 1),
-(211, 'Delta X', '45 mile range', '', 61, 91, '3999', 1),
-(221, 'Quest Max', '40 mile range', '', 61, 91, '2999', 1),
-(231, 'Compass', '50 mile range', '', 61, 91, '3299', 1);
+(21, 'Model 3', '310 mile range, 0-60 mpg 3.5 seconds', 'https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/model3-proto/specs/compare-model3--right.png
+', 21, 51, '45000', 1),
+(31, 'Model S', '315 range, 518 horsepower, 6 hour ', 'https://otomotopl-imagestmp.akamaized.net/images_otomotopl/877108315_1_1080x720_p90d-pakiet-premium-pelna-opcja-dozywotnie-ladowanie-1wl-fvat-warszawa.jpg
+', 21, 51, '77000', 1),
+(41, 'Model X', '289 mile range, 518 horsepower, 0-', 'https://s3.amazonaws.com/myev-data/listing/0001/11/thumb_10594_listing_big.jpeg
+', 21, 51, '83000', 1),
+(51, 'Zero S', 'Top speed 95mph, Range ', 'https://www.plugincars.com/sites/default/files/zero-s-studio-620.jpg
+', 31, 61, '11000', 1),
+(61, 'Zero SR', 'Top speed 95mph', 'https://www.chuckhawks.com/zero_SR.jpg
+', 31, 61, '16500', 1),
+(71, 'Zero DS', 'Top speed 98 mph, Rang', 'https://gearmoose.com/wp-content/uploads/2013/10/2013-zero-ds-electric-motorcycle.jpg
+', 31, 61, '11000', 1),
+(81, 'Zero DSR', '102 mph top speed Range', 'https://www.tflcar.com/wp-content/uploads/2016/03/2016_zero_dsr_01-620x589.jpg
+', 31, 61, '16500', 1),
+(91, 'Zero FX', 'Top Speed 85 mpg, Range:', 'https://www.cyclenews.com/wp-content/uploads/2018/06/2018-zero-fx-2.jpg
+', 31, 61, '9000', 1),
+(101, 'Zero FXS', 'Top Speed 85 mpg, Range ', 'http://media.zeromotorcycles.com/gallery-2016/zero-fxs/location/large/2016_zero-fxs_action-06_777x555_gallery.jpg
+', 31, 61, '9000', 1),
+(111, 'Bamboo GTX Street', 'Top Speed 26mph Ran', 'https://cdn.shopify.com/s/files/1/0869/0934/products/GTX_ST_Main_grande_540x_9d1d0592-53cf-4b96-a9fd-ea9f7576a041_650x.jpg?v=1523890848
+', 41, 71, '1560', 1),
+(121, 'Bamboo One', 'Top Speed 26 Mph Range 21 ', 'https://cdn.shopify.com/s/files/1/0869/0934/products/281a4930-abf3-51fa-95c8-d4112a1264d2_52cd2c6e-50f3-421b-8662-c5e59f5af8eb_900x.png?v=1534787914
+', 41, 71, '800', 1),
+(131, 'GT Carbon Series', 'Top Speed 26mph Rang', 'https://cdn.shopify.com/s/files/1/1472/8356/products/Evolve_Skateboards_Bamboo_GTX_Series_All_Terrain_20_grande.jpg?v=1541045682
+', 41, 71, '1960', 1),
+(141, 'E Prime', 'Electric Scooter 15 mph speed Range ', 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6255/6255464_sd.jpg;maxHeight=640;maxWidth=550
+', 51, 81, '379', 1),
+(151, 'E300', 'Electric Scooter 15 mph speed Range 45 ', 'https://www.razor.com/wp-content/uploads/2018/04/e300_whbl_product-700x1024.png
+', 51, 81, '279', 1),
+(161, 'E150', 'Electric Scooter 10 mph speed, Range 40', 'https://www.razor.com/wp-content/uploads/2018/08/e150_pu_product.png
+', 51, 81, '159', 1),
+(171, 'Eco Smart Metro', 'Electric Scooter 18mph Speed', 'https://images-na.ssl-images-amazon.com/images/I/61W7KDUnXlL._SX679_.jpg
+', 51, 81, '469', 1),
+(181, 'E200S', 'Electric Scooter Seated 12 mph speed R', 'https://www.myproscooter.com/wp-content/uploads/2018/05/41mBN2BGzDeL.jpg
+', 51, 81, '259', 1),
+(191, 'Galaxy ST', '50 mile range', 'https://i0.wp.com/electricbikereport.com/wp-content/uploads/2017/09/EVELO-Galaxy-ST-electric-bike-3.jpg?fit=1200%2C900&ssl=1&w=640
+', 61, 91, '2999', 1),
+(201, 'Aurora', '40 mile range', 'https://www.bestcyclebikes.com/wp-content/uploads/EVELO-Aurora-Electric-Bike-with-Shimano-Alivio-8-Speed-Drivetrain-250W-Mid-Drive-Motor-White-0-0.jpg
+', 61, 91, '2999', 1),
+(211, 'Delta X', '45 mile range', 'https://biketoday.news/storage/articles/1896/1531757872.jpg
+', 61, 91, '3999', 1),
+(221, 'Quest Max', '40 mile range', 'https://www.evelo.com/wp-content/uploads/2017/09/quest-max-steel-gray.jpg
+', 61, 91, '2999', 1),
+(231, 'Compass', '50 mile range', 'https://www.evelo.com/wp-content/uploads/2017/10/compass-featured.jpg
+', 61, 91, '3299', 1);
 
 -- --------------------------------------------------------
 
