d636c431fd71b6e1844210cca187852094aab04f
Corey Johnson <cojohnson@csumb.edu>
2018-12-10 02:14:58 +0000

    product descriptions update

--- heroku_74152a32ba521c4.sql
+++ heroku_74152a32ba521c4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 10, 2018 at 01:14 AM
+-- Generation Time: Dec 10, 2018 at 02:14 AM
 -- Server version: 5.5.57-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.22
 
@@ -133,28 +133,28 @@ CREATE TABLE `f_product` (
 --
 
 INSERT INTO `f_product` (`productID`, `productName`, `productDescription`, `productImage`, `brandID`, `categoryID`, `price`) VALUES
-(21, 'Model 3', '310 mile range, 0-60 mpg 3.5 seconds', 'https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/model3-proto/specs/compare-model3--right.png', 21, 51, '45002.00'),
-(31, 'Model S', '315 range, 518 horsepower, 6 hour ', 'https://otomotopl-imagestmp.akamaized.net/images_otomotopl/877108315_1_1080x720_p90d-pakiet-premium-pelna-opcja-dozywotnie-ladowanie-1wl-fvat-warszawa.jpg\r\n', 21, 51, '77000.00'),
-(41, 'Model X', '289 mile range, 518 horsepower, 0-', 'https://s3.amazonaws.com/myev-data/listing/0001/11/thumb_10594_listing_big.jpeg\r\n', 21, 51, '83000.00'),
-(51, 'Zero S', 'Top speed 95mph, Range ', 'https://www.plugincars.com/sites/default/files/zero-s-studio-620.jpg\r\n', 31, 61, '11000.00'),
-(61, 'Zero SR', 'Top speed 95mph', 'https://www.chuckhawks.com/zero_SR.jpg\r\n', 31, 61, '16500.00'),
-(71, 'Zero DS', 'Top speed 98 mph, Rang', 'https://gearmoose.com/wp-content/uploads/2013/10/2013-zero-ds-electric-motorcycle.jpg\r\n', 31, 61, '11000.00'),
-(81, 'Zero DSR', '102 mph top speed Range', 'https://www.tflcar.com/wp-content/uploads/2016/03/2016_zero_dsr_01-620x589.jpg\r\n', 31, 61, '16500.00'),
-(91, 'Zero FX', 'Top Speed 85 mpg, Range:', 'https://www.cyclenews.com/wp-content/uploads/2018/06/2018-zero-fx-2.jpg\r\n', 31, 61, '9000.00'),
-(101, 'Zero FXS', 'Top Speed 85 mpg, Range ', 'http://media.zeromotorcycles.com/gallery-2016/zero-fxs/location/large/2016_zero-fxs_action-06_777x555_gallery.jpg\r\n', 31, 61, '9000.00'),
-(111, 'Bamboo GTX Street', 'Top Speed 26mph Ran', 'https://cdn.shopify.com/s/files/1/0869/0934/products/GTX_ST_Main_grande_540x_9d1d0592-53cf-4b96-a9fd-ea9f7576a041_650x.jpg?v=1523890848\r\n', 41, 71, '1560.00'),
-(121, 'Bamboo One', 'Top Speed 26 Mph Range 21 ', 'https://cdn.shopify.com/s/files/1/0869/0934/products/281a4930-abf3-51fa-95c8-d4112a1264d2_52cd2c6e-50f3-421b-8662-c5e59f5af8eb_900x.png?v=1534787914\r\n', 41, 71, '800.00'),
-(131, 'GT Carbon Series', 'Top Speed 26mph Rang', 'https://cdn.shopify.com/s/files/1/1472/8356/products/Evolve_Skateboards_Bamboo_GTX_Series_All_Terrain_20_grande.jpg?v=1541045682\r\n', 41, 71, '1960.00'),
-(141, 'E Prime', 'Electric Scooter 15 mph speed Range ', 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6255/6255464_sd.jpg;maxHeight=640;maxWidth=550\r\n', 51, 81, '379.00'),
-(151, 'E300', 'Electric Scooter 15 mph speed Range 45 ', 'https://www.razor.com/wp-content/uploads/2018/04/e300_whbl_product-700x1024.png\r\n', 51, 81, '279.00'),
-(161, 'E150', 'Electric Scooter 10 mph speed, Range 40', 'https://www.razor.com/wp-content/uploads/2018/08/e150_pu_product.png\r\n', 51, 81, '159.00'),
-(171, 'Eco Smart Metro', 'Electric Scooter 18mph Speed', 'https://images-na.ssl-images-amazon.com/images/I/61W7KDUnXlL._SX679_.jpg\r\n', 51, 81, '469.00'),
-(181, 'E200S', 'Electric Scooter Seated 12 mph speed R', 'https://www.myproscooter.com/wp-content/uploads/2018/05/41mBN2BGzDeL.jpg\r\n', 51, 81, '259.00'),
-(191, 'Galaxy ST', '50 mile range', 'https://i0.wp.com/electricbikereport.com/wp-content/uploads/2017/09/EVELO-Galaxy-ST-electric-bike-3.jpg?fit=1200%2C900&ssl=1&w=640\r\n', 61, 91, '2999.00'),
-(201, 'Aurora', '40 mile range', 'https://www.bestcyclebikes.com/wp-content/uploads/EVELO-Aurora-Electric-Bike-with-Shimano-Alivio-8-Speed-Drivetrain-250W-Mid-Drive-Motor-White-0-0.jpg\r\n', 61, 91, '2999.00'),
-(211, 'Delta X', '45 mile range', 'https://biketoday.news/storage/articles/1896/1531757872.jpg\r\n', 61, 91, '3999.00'),
-(221, 'Quest Max', '40 mile range', 'https://www.evelo.com/wp-content/uploads/2017/09/quest-max-steel-gray.jpg\r\n', 61, 91, '2999.00'),
-(231, 'Compass', '50 mile range', 'https://www.evelo.com/wp-content/uploads/2017/10/compass-featured.jpg\r\n', 61, 91, '3299.00');
+(21, 'Model 3', 'Model 3 has a 310 mile range, 0-60 mph acceleration in 3.3 seconds', 'https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/model3-proto/specs/compare-model3--right.png', 21, 51, '45002.00'),
+(31, 'Model S', 'Model S has a 315 mile range, 0-60 mph acceleration in 2.5 seconds', 'https://otomotopl-imagestmp.akamaized.net/images_otomotopl/877108315_1_1080x720_p90d-pakiet-premium-pelna-opcja-dozywotnie-ladowanie-1wl-fvat-warszawa.jpg', 21, 51, '77000.00'),
+(41, 'Model X', 'Model X has a 295 mile range, 0-60 mph acceleration in 2.9 seconds', 'https://s3.amazonaws.com/myev-data/listing/0001/11/thumb_10594_listing_big.jpeg', 21, 51, '83000.00'),
+(51, 'Zero S ZF14.4', 'Zero S ZF14.4  has a range of 90/179 (highway/city) with a top speed of 98 mph', 'https://www.plugincars.com/sites/default/files/zero-s-studio-620.jpg', 31, 61, '13995.00'),
+(61, 'Zero SR', 'Zero SR has a range of 112/223 (highway/city) with a top speed of 102 mph', 'https://www.chuckhawks.com/zero_SR.jpg', 31, 61, '16495.00'),
+(71, 'Zero DS ZF14.4', 'Zero DS ZF14.4 has a range of 78/163 (highway/city) with a top speed of 98 mph', 'https://gearmoose.com/wp-content/uploads/2013/10/2013-zero-ds-electric-motorcycle.jpg', 31, 61, '13995.00'),
+(81, 'Zero DSR', 'Zero DSR has a range of 78/163 (highway/city) with a top speed of 102 mph', 'https://www.tflcar.com/wp-content/uploads/2016/03/2016_zero_dsr_01-620x589.jpg', 31, 61, '16495.00'),
+(91, 'Zero FX ZF7.2', 'Zero FX ZF7.2 has a range of 39/91 (highway/city) with a top speed of 85 mph', 'https://www.cyclenews.com/wp-content/uploads/2018/06/2018-zero-fx-2.jpg', 31, 61, '10495.00'),
+(101, 'Zero FXS ZF7.2', 'Zero FXS ZF7.2 has a range of 40/100 (highway/city) with a top speed of 85 mph', 'http://media.zeromotorcycles.com/gallery-2016/zero-fxs/location/large/2016_zero-fxs_action-06_777x555_gallery.jpg', 31, 61, '10495.00'),
+(111, 'Bamboo GTX Street', 'The Bamboo GTX Street has a range up to 31 miles with a top speed of 26 mph', 'https://cdn.shopify.com/s/files/1/0869/0934/products/GTX_ST_Main_grande_540x_9d1d0592-53cf-4b96-a9fd-ea9f7576a041_650x.jpg?v=1523890848', 41, 71, '1560.00'),
+(121, 'Bamboo One', 'The Bamboo One has a range up to 21 miles with a top speed of 22 mph', 'https://cdn.shopify.com/s/files/1/0869/0934/products/281a4930-abf3-51fa-95c8-d4112a1264d2_52cd2c6e-50f3-421b-8662-c5e59f5af8eb_900x.png?v=1534787914', 41, 71, '999.00'),
+(131, 'GT Carbon Series', 'The GT Carbon Series has a range up to 31 miles with a top speed of 26 mph', 'https://cdn.shopify.com/s/files/1/1472/8356/products/Evolve_Skateboards_Bamboo_GTX_Series_All_Terrain_20_grande.jpg?v=1541045682', 41, 71, '1959.99'),
+(141, 'E Prime', 'The Razor E Prime Scooter has a top speed of 15 mph and 40 minute battery life', 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6255/6255464_sd.jpg;maxHeight=640;maxWidth=550', 51, 81, '379.00'),
+(151, 'E300', 'The Razor E300 Scooter has a top speed of 15 mph and 40 minute battery life', 'https://www.razor.com/wp-content/uploads/2018/04/e300_whbl_product-700x1024.png', 51, 81, '255.99'),
+(161, 'E150', 'The Razor E150 Scooter has a top speed of 10 mph and 40 minute battery life', 'https://www.razor.com/wp-content/uploads/2018/08/e150_pu_product.png', 51, 81, '159.00'),
+(171, 'Eco Smart Metro', 'The Eco Smart Metro Scooter has a top speed of 18 mph and 40 minute battery life', 'https://images-na.ssl-images-amazon.com/images/I/61W7KDUnXlL._SX679_.jpg', 51, 81, '349.99'),
+(181, 'E200S', 'The Razor E200S has a top speed of 12 mph and a battery life of 45 minutes', 'https://www.myproscooter.com/wp-content/uploads/2018/05/41mBN2BGzDeL.jpg', 51, 81, '259.99'),
+(191, 'Galaxy ST', 'The Galaxy ST has a top speed of 20 mph and range of 30/50 miles (electric/assisted)', 'https://i0.wp.com/electricbikereport.com/wp-content/uploads/2017/09/EVELO-Galaxy-ST-electric-bike-3.jpg?fit=1200%2C900&ssl=1&w=640', 61, 91, '2999.00'),
+(201, 'Aurora', 'The Aurora has a top speed of 20 mph and range of 20/40 miles (electric/assisted)', 'https://www.bestcyclebikes.com/wp-content/uploads/EVELO-Aurora-Electric-Bike-with-Shimano-Alivio-8-Speed-Drivetrain-250W-Mid-Drive-Motor-White-0-0.jpg', 61, 91, '2999.00'),
+(211, 'Delta X', 'The Delta X has a top speed of 20 mph and range of 20/45 miles (electric/assisted)', 'https://biketoday.news/storage/articles/1896/1531757872.jpg', 61, 91, '3999.00'),
+(221, 'Quest Max', 'The Quest Max has a top speed of 20 mph and range of 20/40 miles (electric/assisted)', 'https://www.evelo.com/wp-content/uploads/2017/09/quest-max-steel-gray.jpg', 61, 91, '2999.00'),
+(231, 'Compass', 'The Compass has a top speed of 18 mph and range of 30/50 miles (electric/assisted)', 'https://www.evelo.com/wp-content/uploads/2017/10/compass-featured.jpg', 61, 91, '3299.00');
 
 --
 -- Indexes for dumped tables
